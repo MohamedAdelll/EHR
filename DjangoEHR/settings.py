a7511a9d20cc4503a79b7a7a176f7ed5b87a8d79
mohamed ashour <ashour521@gmail.com>
2024-03-22 17:19:50 +0200

    modifying database parameters

--- DjangoEHR/settings.py
+++ DjangoEHR/settings.py
@@ -78,25 +78,25 @@ WSGI_APPLICATION = 'DjangoEHR.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 # version 1
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-   }
-
-# # version2
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'lab3database',
-#         'PASSWORD': 'Mypassword3',
-#         'HOST': 'ehrserver.postgres.database.azure.com',
-#         'PORT': '5432',
-#         'OPTIONS':{'sslmode':'require'},
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
-# }
+#    }
+
+# version2
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ass1-dist-lab',
+        'USER': 'mohamed',
+        'PASSWORD': '23633144Aa!',
+        'HOST': 'ehrserver.postgres.database.azure.com',
+        'PORT': '5432',
+        'OPTIONS':{'sslmode':'require'},
+    }
+}
 
 
 # Password validation
